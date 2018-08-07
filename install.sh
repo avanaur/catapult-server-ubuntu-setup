9ff31d4a044eec9ffe88d456095a97cddb906ac0
xgreenx <xgreenx9999@gmail.com>
2018-08-07 16:20:45 +0300

    Move install dependency to own script

--- install.sh
+++ install.sh
@@ -1,73 +1,5 @@
-sed -i.bak -e "s%http://[^ ]\+%http://linux.yz.yamagata-u.ac.jp/ubuntu/%g" /etc/apt/sources.list
-apt-get update -y && apt-get upgrade -y && apt-get clean && apt-get install -y --no-install-recommends \
-  git \
-  curl \
-  wget \
-  vim \
-  autoconf \
-  automake \
-  apt-file \
-  build-essential \
-  software-properties-common \
-  pkg-config \
-  python3 \
-  libc6-dev \
-  libssl-dev \
-  libsasl2-dev \
-  libtool \
-  && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-add-apt-repository ppa:ubuntu-toolchain-r/test \
-  && apt-get update && apt-get install -y --no-install-recommends gcc-7 g++-7 \
-  && apt-get clean && rm -rf /var/lib/apt/lists/* \
-  && rm /usr/bin/gcc /usr/bin/g++ \
-  && ln -s /usr/bin/gcc-7 /usr/bin/gcc \
-  && ln -s /usr/bin/g++-7 /usr/bin/g++
-
-git clone https://gitlab.kitware.com/cmake/cmake.git -b v3.11.1 --depth 1 \
-  && cd cmake \
-  && ./bootstrap --prefix=/usr/local && make -j4 && make install \
-  && cd -
-
-wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz \
-  && tar xzf boost_1_65_1.tar.gz && cd boost_1_65_1 \
-  && ./bootstrap.sh && ./b2 toolset=gcc install --prefix=/usr/local -j4 \
-  && rm boost_1_65_1.tar.gz && cd -
-
-git clone https://github.com/google/googletest.git -b release-1.8.0 --depth 1 \
-  && mkdir -p googletest/_build && cd googletest/_build \
-  && cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j4 && make install \
-  && cd -
-
-git clone https://github.com/facebook/rocksdb.git -b v5.12.4 --depth 1 \
-  && mkdir -p rocksdb/_build && cd rocksdb/_build \
-  && cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j4 && make install \
-  && cd -
-
-git clone https://github.com/zeromq/libzmq.git -b v4.2.3 --depth 1 \
-  && mkdir -p libzmq/_build && cd libzmq/_build \
-  && cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j4 && make install \
-  && cd -
-
-git clone https://github.com/zeromq/cppzmq.git -b v4.2.3 --depth 1 \
-  && mkdir -p cppzmq/_build && cd cppzmq/_build \
-  && cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. && make -j4 && make install \
-  && cd -
-
-git clone https://github.com/mongodb/mongo-c-driver.git -b 1.4.3 --depth 1 && cd mongo-c-driver \
-  && ./autogen.sh && ./configure --disable-automatic-init-and-cleanup --prefix=/usr/local \
-  && make -j4 && make install \
-  && cd -
-#RUN wget https://github.com/mongodb/mongo-c-driver/releases/download/1.4.2/mongo-c-driver-1.4.2.tar.gz \
-#  && tar xzf mongo-c-driver-1.4.2.tar.gz && cd mongo-c-driver-1.4.2 \
-#  && ./configure --disable-automatic-init-and-cleanup --prefix=/usr/local \
-#  && make -j4 && make install \
-#  && cd -
-
-git clone https://github.com/mongodb/mongo-cxx-driver.git -b r3.0.2 --depth 1 && cd mongo-cxx-driver/build \
-  && cmake -DBSONCXX_POLY_USE_BOOST=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local .. \
-  && make -j4 && make install \
-  && cd -
+sudo bash ./installDependency.sh
 
 git clone https://github.com/nemtech/catapult-server.git -b master --depth 1 \
   && cd catapult-server \
@@ -90,4 +22,3 @@ cd catapult-server \
   && cp -r ../resources . \
   && ./bin/catapult.tools.nemgen ../tools/nemgen/resources/mijin-test.properties \
   && cp -r ../seed/mijin-test/00000 ../data/
-
